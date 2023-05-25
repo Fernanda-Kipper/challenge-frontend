cae047886295522eaafe2e421a0ceac9b39d65b5
Fernanda Kipper <nanda.kipper@gmail.com>
2023-05-24 21:26:25 -0300

    refactor: extract providers

--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -1,19 +1,15 @@
 "use client"
 
-import Image from 'next/image'
 import styles from './page.module.css'
 import { FilterBar } from '@/components/filter-bar'
 import { ProductsList } from '@/components/products-list'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function Home() {
-  const client = new QueryClient();
+
   return (
-    <QueryClientProvider client={client}>
       <main className={styles.main}>
         <FilterBar/>
         <ProductsList/>
       </main>
-    </QueryClientProvider>
   )
 }
---
+++ capputeeno/src/components/default-providers.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { FilterContextProvider } from "@/contexts/filter-context";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+import { ThemeProvider } from "styled-components";
+
+interface DefaultProvidersProps {
+    children: ReactNode
+}
+
+const theme = {
+    desktopBreakpoint: "768px"
+}
+
+export function DefaultProviders({ children } : DefaultProvidersProps){
+    const client = new QueryClient();
+    return(
+        <QueryClientProvider client={client}>
+            <FilterContextProvider>
+                <ThemeProvider theme={theme}>
+                    {children}
+                </ThemeProvider>
+            </FilterContextProvider>
+        </QueryClientProvider>
+    )
+}
