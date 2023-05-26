84ff0df4ec2fffa1401d8befc4a6f4760fda53e8
Fernanda Kipper <nanda.kipper@gmail.com>
2023-05-26 13:40:42 -0300

    refactor: extract divider component

---
+++ capputeeno/src/components/divider.tsx
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+export const Divider = styled.span`
+    width: 100%;
+    height: 1px;
+    margin: 8px auto;
+    padding: 0px;
+    background: var(--shapes);
+`
--- capputeeno/src/components/product-card.tsx
+++ capputeeno/src/components/product-card.tsx
@@ -1,6 +1,7 @@
 import { formatPrice } from "@/utils/format-price"
 import { useRouter } from "next/navigation"
 import { styled } from "styled-components"
+import { Divider } from "./divider"
 
 interface ProductCardProps {
     image: string,
@@ -46,15 +47,8 @@ const Card = styled.div`
         align-items: start;
         justify-content: center;
         flex-direction: column;
-        padding: 8px 0;
-
-        > div {
-            width: 228px;
-            height: 1px;
-            margin: 8px 0;
-            padding: 0px;
-            background: var(--shapes);
-        }
+        padding: 8px 12px;
+        width: 100%;
     }
 `
 
@@ -71,7 +65,7 @@ export function ProductCard(props : ProductCardProps){
             <img src={props.image}/>
             <div>
                 <h3>{props.title}</h3>
-                <div></div>
+                <Divider/>
                 <p>{price}</p>
             </div>
         </Card>
